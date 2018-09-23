59b03200484c5b7ffea565d5701d1c5e39d73826
gujjurocks <41317723+gujjurocks@users.noreply.github.com>
2018-09-23 22:20:39 +1000

    RecordServiceCTL Testing file
    Integration Testing file for RecordServiceCTL class

---
+++ Asg3 Resources/Resources/src/hotel/service/RecordServiceCTLTest.java
@@ -0,0 +1,24 @@
+package hotel.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecordServiceCTLTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testServiceDetailsEntered() {
+		//fail("Not yet implemented");
+	}
+
+}
