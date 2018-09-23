a039b54b9e8545a8688bcb612127b466218fe138
gujjurocks <41317723+gujjurocks@users.noreply.github.com>
2018-09-23 16:55:53 +0530

    Room Test File
    i have use Junit test method to test the class

---
+++ Asg3 Resources/Resources/src/hotel/entities/RoomTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package hotel.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Vismay
+ *
+ */
+public class RoomTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link hotel.entities.Room#book(hotel.entities.Guest, java.util.Date, int, int, hotel.credit.CreditCard)}.
+	 */
+	@Test
+	public void testBook() {
+		//fail("Not yet implemented");
+	}
+
+}
