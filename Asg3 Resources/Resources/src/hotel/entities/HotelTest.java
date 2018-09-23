28b976983250afd11ee9e9d9249fbcd418e649cc
gujjurocks <41317723+gujjurocks@users.noreply.github.com>
2018-09-23 22:35:16 +1000

    Hotel JUnit testing
    I did testing for Hotel class and others methods using JUnit testing.

---
+++ Asg3 Resources/Resources/src/hotel/entities/HotelTest.java
@@ -0,0 +1,39 @@
+package hotel.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HotelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBook() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckin() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddServiceCharge() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckout() {
+		//fail("Not yet implemented");
+	}
+
+}
